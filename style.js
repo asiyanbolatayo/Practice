eda5879537a8e41e18f8caf83938205a504ebe7c
Muhammed Asiyanbola <asiyanbolamuhammed.mb@gmail.com>
2024-12-29 14:12:05 +0100

    updated codes

--- style.js
+++ style.js
@@ -16,3 +16,22 @@ function light(sw) {
     document.getElementById("demo").style.backgroundColor = "yellow";        
   }
   
+  var w;
+
+function startWorker() {
+  if (typeof(Worker) !== "undefined") {
+    if (typeof(w) == "undefined") {
+      w = new Worker("demo_workers.js");
+    }
+    w.onmessage = function(event) {
+      document.getElementById("result").innerHTML = event.data;
+    };
+  } else {
+    document.getElementById("result").innerHTML = "Sorry! No Web Worker support.";
+  }
+}
+
+function stopWorker() {
+  w.terminate();
+  w = undefined;
+}
